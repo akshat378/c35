97452eaf51b896c7233aca93e0b8a2b4493b4a38
akshat378 <62461557+akshat378@users.noreply.github.com>
2020-07-04 13:50:58 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,16 @@
+class Form{
+constructor(){
+
+
+}
+display(){
+var title=createElement("h2")
+title.html("Car Racing Game" )
+title.position(130,0) 
+var input= createInput("name")
+input.position(450,200)    
+ var button=createButton ("play")
+ button.position(500,300)
+}
+
+}
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var form;
+
+function setup(){
+ 
+  createCanvas(500,500);
+   form=new Form();
+}
+
+function draw(){
+  background("white");
+  form.display();
+    
+    
+  
+}
+
+
+  
